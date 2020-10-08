06057d50b930a77ebd11fa8c32d889bf53f92a69
datasg <allison.m.gottlieb@gmail.com>
2020-10-07 21:33:11 -0400

    working on useState

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,48 @@
 import React, {useState} from "react";
 
 const Header = (props) => {
-    const [inputName, setInputName] = useState ({submittedName: ''})
+    const [inputName, setInputName] = useState ( {submittedName: ''} )
+ 
+    const submitHandler = event => {
+    event.preventDefault();
+    // ...
+  };
 
   return (
   <header className="header">
     <h1>Custom License Plate Printer</h1>
     <aside>
-      <input type="text" 
+      <input 
+      type="text" 
       placeholder="My name is..." 
+      id= "submittedName"
       className="search" 
       value={inputName.submittedName}
-      onChange ={setInputName}/>
-      <input type="submit" value="Create License Plate" />
+      onChange = {setInputName}
+      />
+      <input type="submit" 
+      value="Create License Plate" 
+      onClick={submitHandler}/>
     </aside>
   </header>
     );
 };
 export default Header;
+
+/* 
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input 
+            type="text" 
+            id="title" 
+            value={inputState.title} 
+            onChange={event => {
+              const newTitle = event.target.value;
+                setInputState(prevInputState => ({
+                  title:newTitle, 
+                  amount: prevInputState.amount
+                  })
+                )} 
+            }/>
+*/
