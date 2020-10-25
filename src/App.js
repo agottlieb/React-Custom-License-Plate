739fba933822393cd7ebc5ddf4d91b68bbe88862
datasg <allison.m.gottlieb@gmail.com>
2020-10-25 15:03:34 -0400

    created useEffect function to save names to local storage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PlatesList from "./PlatesList.js";
 import Header from "./Header.js";
 
 const App = () => {
-  const [userAddedPlates, setUserAddedPlates] = useState(["Bort", "Waylon"]);
+  const [userAddedPlates, setUserAddedPlates] = useState(["Bort", "Waylon"])
+ // const savedNames = () => window.localStorage.getItem('savedNames')
 
   const addPlate = (plate) => {
     const newPlates = [plate, ...userAddedPlates];
     setUserAddedPlates(newPlates)
-    // useEffect( () => {
-    //   window.localStorage.setItem('plate', plate)
-    // })
   };
 
-  //value is never called but needs to be defined in order to get to index (a second variable)
+//Saved items in local storage to show up when app renders/ComponentDidMount
+  useEffect( () => {
+       window.localStorage.setItem('savedNames', userAddedPlates)
+      })
+
+
+  //"value" is never called but needs to be defined in order to get to index (a second variable)
      const removeNameHandler = (clickedIndex) => {
       const filteredValues = userAddedPlates.filter((value, index) => {
         return index !== clickedIndex
       })
-      
       setUserAddedPlates(filteredValues);
     };   
 
-//Saved items in local storage to show up when app renders
- 
 
 //Handler to saving items to local storage
 //     const saveLocal = useEffect( () => {window.localStorage.setItem('plate', plate)})
