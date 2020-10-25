db5d1143c4a352d42a8dd73b900a75c15c11dd27
datasg <allison.m.gottlieb@gmail.com>
2020-10-25 15:25:36 -0400

    attempt to get local storage names to load on start

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import PlatesList from "./PlatesList.js";
 import Header from "./Header.js";
 
 const App = () => {
-  const [userAddedPlates, setUserAddedPlates] = useState(["Bort", "Waylon"])
- // const savedNames = () => window.localStorage.getItem('savedNames')
+  //savedNames is function so it is only rendered when useState is rendered the first time
+  const savedNames = () => window.localStorage.getItem(JSON.parse('savedNames') )|| ""
+
+  const [userAddedPlates, setUserAddedPlates] = useState(["Bort", "Waylon"], savedNames)
+
+  //Saved items in local storage to show up when app renders 
+  //second value is called so it only runs when userAddedPlates is updated
+  useEffect( () => {
+    const savedNames =JSON.stringify(userAddedPlates)
+     window.localStorage.setItem('savedNames', savedNames)
+    }, [userAddedPlates])  
 
   const addPlate = (plate) => {
     const newPlates = [plate, ...userAddedPlates];
     setUserAddedPlates(newPlates)
   };
-
-//Saved items in local storage to show up when app renders/ComponentDidMount
-  useEffect( () => {
-       window.localStorage.setItem('savedNames', userAddedPlates)
-      })
-
 
   //"value" is never called but needs to be defined in order to get to index (a second variable)
      const removeNameHandler = (clickedIndex) => {
